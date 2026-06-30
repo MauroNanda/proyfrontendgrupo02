92b6177ce7054ef402eadd04aa925deeb12ca3ac
unknown <deliacusipuma80@gmail.com>
2026-06-30 13:23:13 -0300

    feat: agregar catálogo de eventos

---
+++ src/app/features/public/event-catalog/event-catalog.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { EventoService, Evento } from '../../../core/services/evento.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-event-catalog',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './event-catalog.component.html',
+  styleUrl: './event-catalog.component.css',
+})
+export class EventCatalogComponent implements OnInit {
+  eventos: Evento[] = [];
+  eventosFiltrados: Evento[] = [];
+
+  filtroEstado = '';
+
+  constructor(private eventoService: EventoService) {}
+
+  ngOnInit(): void {
+    this.eventoService.obtenerTodos().subscribe({
+      next: (data) => {
+        this.eventos = data;
+        this.eventosFiltrados = data;
+      },
+      error: (err) => {
+        console.log('Error cargando eventos', err);
+      },
+    });
+  }
+
+  filtrarEventos() {
+    if (!this.filtroEstado) {
+      this.eventosFiltrados = this.eventos;
+      return;
+    }
+
+    this.eventosFiltrados = this.eventos.filter((evento) => evento.estado === this.filtroEstado);
+  }
+}
